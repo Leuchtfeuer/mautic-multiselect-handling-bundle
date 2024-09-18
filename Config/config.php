b9a9123ed4f8eae87af1e0a63df043d34b6151de
Patrick Jenkner <p.jenkner@leuchtfeuer.com>
2024-09-18 14:26:40 +0200

    apply cs fixer findings

--- Config/config.php
+++ Config/config.php
@@ -8,40 +8,40 @@ return [
     'services'    => [
         'other' => [
             'mautic.plugin.multiselect_handling.lead_field_choice_loader' => [
-                'class'     => \MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\Form\Loader\LeadFieldChoiceLoader::class,
+                'class'     => MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\Form\Loader\LeadFieldChoiceLoader::class,
                 'arguments' => [
                     'mautic.lead.repository.field',
                 ],
             ],
             'mautic.plugin.multiselect_handling.lead_field_values_choice_loader' => [
-                'class'     => \MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\Form\Loader\LeadFieldValuesChoiceLoader::class,
+                'class'     => MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\Form\Loader\LeadFieldValuesChoiceLoader::class,
                 'arguments' => [
                     'mautic.lead.repository.field',
                 ],
             ],
             'mautic.plugin.multiselect_handling.model.segments' => [
-                'class'     => \MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\Model\SegmentsModel::class,
+                'class'     => MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\Model\SegmentsModel::class,
                 'arguments' => [
                     'mautic.lead.model.list',
                     'mautic.plugin.multiselect_handling.lead_field_choice_loader',
                 ],
             ],
             'mautic.leuchtfeuermultiselect.config' => [
-                'class'     => \MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\Integration\Config::class,
+                'class'     => MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\Integration\Config::class,
                 'arguments' => [
                     'mautic.integrations.helper',
                     ],
                 ],
             ],
         'events' => [
-            \MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\EventListener\FormSubscriber::class => [
-                'class'     => \MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\EventListener\FormSubscriber::class,
+            MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\EventListener\FormSubscriber::class => [
+                'class'     => MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\EventListener\FormSubscriber::class,
                 'arguments' => [
                     'mautic.leuchtfeuermultiselect.config',
                 ],
             ],
-            \MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\EventListener\FormAction::class => [
-                'class'     => \MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\EventListener\FormAction::class,
+            MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\EventListener\FormAction::class => [
+                'class'     => MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\EventListener\FormAction::class,
                 'arguments' => [
                     'mautic.leuchtfeuermultiselect.config',
                     'mautic.plugin.multiselect_handling.lead_field_choice_loader',
@@ -50,16 +50,16 @@ return [
                     'mautic.plugin.multiselect_handling.model.segments',
                 ],
             ],
-            \MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\EventListener\ActionSubscriber::class => [
-                'class'     => \MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\EventListener\ActionSubscriber::class,
+            MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\EventListener\ActionSubscriber::class => [
+                'class'     => MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\EventListener\ActionSubscriber::class,
                 'arguments' => [
                     'mautic.leuchtfeuermultiselect.config',
                     'mautic.lead.model.lead',
                     'mautic.plugin.multiselect_handling.model.segments',
                 ],
             ],
-            \MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\EventListener\CampaignSubscriber::class => [
-                'class'     => \MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\EventListener\CampaignSubscriber::class,
+            MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\EventListener\CampaignSubscriber::class => [
+                'class'     => MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\EventListener\CampaignSubscriber::class,
                 'arguments' => [
                     'mautic.leuchtfeuermultiselect.config',
                 ],
@@ -67,13 +67,13 @@ return [
         ],
         'forms' => [
             'mautic.plugin.multiselect_handling.action_settings_type' => [
-                'class'     => \MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\Form\Type\SettingsType::class,
+                'class'     => MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\Form\Type\SettingsType::class,
                 'arguments' => [
                     'mautic.plugin.multiselect_handling.lead_field_choice_loader',
                 ],
             ],
             'mautic.plugin.multiselect_handling.update_multiselect_field_type' => [
-                'class'     => \MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\Form\Type\UpdateSelectFieldType::class,
+                'class'     => MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\Form\Type\UpdateSelectFieldType::class,
                 'arguments' => [
                     'mautic.plugin.multiselect_handling.lead_field_choice_loader',
                     'mautic.plugin.multiselect_handling.lead_field_values_choice_loader',
@@ -82,14 +82,14 @@ return [
         ],
         'integrations' => [
             'mautic.integration.leuchtfeuermultiselect' => [
-                'class' => \MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\Integration\LeuchtfeuerMultiselectIntegration::class,
+                'class' => MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\Integration\LeuchtfeuerMultiselectIntegration::class,
                 'tags'  => [
                     'mautic.integration',
                     'mautic.basic_integration',
                 ],
             ],
             'leuchtfeuermultiselect.integration.configuration' => [
-                'class' => \MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\Integration\Support\ConfigSupport::class,
+                'class' => MauticPlugin\LeuchtfeuerMultiselectHandlingBundle\Integration\Support\ConfigSupport::class,
                 'tags'  => [
                     'mautic.config_integration',
                 ],
